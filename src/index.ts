1a668901d1d2733981472d1638a0415c4f50134a
NoureddineHelb <nbozanih@helb-prigogine.be>
2024-02-06 13:34:11 +0100

    bot in console working

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import { router } from './Routes/chat.route';
 import  * as dotenv from 'dotenv';
 
 dotenv.config({ path: '.env' });
 const app = express();
 const PORT = process.env.SERVER_PORT;
+const FrontendPORT = process.env.FRONTEND_PORT;
 
+app.use(cors({ origin: 'http://localhost:4200' }));
 app.use(express.json());
 app.use('/api', router);
 
