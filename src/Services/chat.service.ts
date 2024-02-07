892b593c9204813eee14c9921ac6de28f0c27f33
hicham-assou <hassou@helb-prigogine.be>
2024-02-07 12:37:02 -0800

    historique bot ok

--- src/Services/chat.service.ts
+++ src/Services/chat.service.ts
@@ -15,7 +15,8 @@ export class ChatService {
         let newMessage:string = '';
         if (listMessages.length != 0){
             newMessage = 'je vais te donner un nouveau prompt et l\'historique de notre conversation et j\'aimerai que tu me reponde sans reprendre tout l\'historique,' +
-                ' chaque message est deliimité par + \'<<>>\'. voici l\'historique : '+ listMessages + ' et voici le nouveau prompt auquel il faut repondre : '
+                ' chaque message est deliimité par + \'<<>>\'. voici l\'historique : '+ listMessages + '. Il faut avoir une conversation fluide comme si je parlais à un humain,' +
+                ' voici le nouveau prompt auquel il faut repondre,: '
                 +'<< ' + message + ' >>';
         }else
             newMessage = message
@@ -26,6 +27,8 @@ export class ChatService {
             temperature: 0,
         });
         listMessages.push(' << you : ' + response.choices[0].message.content + '>>')
+
+        console.log("=> " + listMessages)
 
         return response.choices[0].message.content;
     }
