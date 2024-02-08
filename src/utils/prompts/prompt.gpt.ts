6a678e26c8d61a0fab9e8f7ca4d1367e88e9fed7
NoureddineHelb <nbozanih@helb-prigogine.be>
2024-02-08 10:18:22 +0100

    :correct: prompt

--- src/utils/prompts/prompt.gpt.ts
+++ src/utils/prompts/prompt.gpt.ts
@@ -1,22 +1,11 @@
-
-
 const promptMessage = (listMessages:string[], message:string) =>{
     return 'je vais te donner un nouveau prompt et l\'historique de notre conversation et j\'aimerai ' +
-        'que tu me reponde sans reprendre tout l\'historique, chaque message est deliimité par + \'<<>>\'.' +
+        'que tu me reponde sans reprendre tout l\'historique, chaque message est delimité par + \'<<>>\'.' +
         'voici l\'historique : '+ listMessages + '. Il faut avoir une conversation fluide comme si je parlais à un humain,' +
-        '. voici le nouveau message auquel il faut repondre,: "' + message + '". Ne met pas les "<<" ou ">>" dans tes messages et contente toi' +
-        ' seulement de répondre';;
-};
-
-const promptMessageWithRole = (listMessages:string[], message:string, role: string) =>{
-    return role + '\nje vais te donner un nouveau prompt et l\'historique de notre conversation et j\'aimerai ' +
-    'que tu me reponde sans reprendre tout l\'historique, chaque message est deliimité par + \'<<>>\'.' +
-    'voici l\'historique : '+ listMessages + '. Il faut avoir une conversation fluide comme si je parlais à un humain,' +
-    '. voici le nouveau message auquel il faut repondre,: "' + message + '". Ne met pas les "<<" ou ">>" dans tes messages et contente toi' +
-    ' seulement de répondre';
+        '. voici le nouveau message auquel tu dois repondre dans la langue du dernier message, n\'hesite pas achnger de langue si c\'est nécessaire: "' + message + '". Ne met pas les "<<" ou ">>" dans tes messages et contente toi' +
+        ' seulement de répondre';
 };
 
 export default {
     promptMessage,
-    promptMessageWithRole
 };
